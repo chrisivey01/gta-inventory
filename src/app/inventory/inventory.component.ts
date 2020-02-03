c6530068ec1b0ca04b0b06cb0aae909e3cae1a14
chris <chrisivey01@gmail.com>
2020-02-02 22:31:49 -0600

    update with images

--- src/app/inventory/inventory.component.ts
+++ src/app/inventory/inventory.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inventory.component.css']
 })
 export class InventoryComponent implements OnInit {
-
+  usersInventory: any[] = new Array(25);
+  otherInventory: any[] = new Array(25);
+  item: {};
+  fileName:string;
+  receivedItems: any[]
   constructor() { }
 
   ngOnInit() {
+    this.receivedItems = [ 
+        {
+          name: "beer",
+          count:5,
+          total:5
+        },
+        {
+          name: "powerade",
+          count:5,
+          total:5
+        },
+      ]
+      this.usersInventory = [...this.receivedItems, ...this.usersInventory].splice(0,25)
+    }
   }
 
-}
